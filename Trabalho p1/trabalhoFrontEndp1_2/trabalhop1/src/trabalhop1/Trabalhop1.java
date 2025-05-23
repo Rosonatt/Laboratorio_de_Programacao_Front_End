377c0312ad8e8581542bb40ee680edff751a7307
Rosonatt Ferreira <h2oroso@gmail.com>
2025-05-23 12:25:08 -0300

    netbeans

---
+++ Trabalho p1/trabalhoFrontEndp1_2/trabalhop1/src/trabalhop1/Trabalhop1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalhop1;
+
+/**
+ *
+ * @author Ryan Guiwison
+ */
+public class Trabalhop1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
